5d7b57eced0388eeedf50241ed312593fc4a9581
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-08-28 20:14:42 +0200

    removed ldflags

--- max/max.go
+++ max/max.go
@@ -1,8 +1,6 @@
 package max
 
 // #cgo CFLAGS: -I${SRCDIR}/../max-sdk/source/c74support/max-includes
-// #cgo LDFLAGS: -L${SRCDIR}/../max-sdk/source/c74support/max-includes
-// #cgo linux LDFLAGS: -Wl,-unresolved-symbols=ignore-all
 // #cgo darwin LDFLAGS: -Wl,-undefined,dynamic_lookup
 // #include "max.h"
 import "C"
